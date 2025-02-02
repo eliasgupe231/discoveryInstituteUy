8e081a6fd3b9b4f05d2a9d296b7107f29d6ae225
Elias Pereira <eliaspereira5566@gmail.com>
2025-02-02 13:17:42 -0300

    2.2v8

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import Header from './components/Header/Header'
 import Nosotros from './pages/Nosotros'
 import Cursos from './pages/Cursos'
-import { Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Inicio from './pages/Inicio'
+
 
 
 function App(){
     return (
         < >
         <Header/>
-        <Routes>
-            <Route path = "/" element = {<Inicio/>}/>
-            <Route path = "./pages/Nosotros.jsx" element = {<Nosotros/>}/>
-            <Route path = "./pages/Cursos.jsx" element = {<Cursos/>}/>
-        </Routes>
+        <Router>
+            <Routes>
+                <Route path = "/" element = {<Inicio/>}/>
+                <Route path = "./pages/Nosotros.jsx" element = {<Nosotros/>}/>
+                <Route path = "./pages/Cursos.jsx" element = {<Cursos/>}/>
+            </Routes>
+        </Router>
         </>
     )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
-})
+  base: '/discoveryInstituteUy/',  // Sustituye 'nombre-repo' con el nombre de tu repositorio en GitHub
+  server: {
+    port: 3000,
+  },
+});
+
